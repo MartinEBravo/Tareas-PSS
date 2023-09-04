e3c6d072fee5252ac42e8e79cb8314364a3fb846
MartinEBravo <martin.bravo@ug.uchile.cl>
2023-09-04 14:49:18 -0300

    tarea2 lista

--- Tarea 2/elim.c
+++ Tarea 2/elim.c
@@ -4,9 +4,6 @@
 
 #include "elim.h"
 
-void eliminar(char *str, char *pat);
-char *eliminados(char *str, char *pat);
-
 // Elimina todas las ocurrencias de pat en str
 void eliminar(char *str, char *pat){
   int len = strlen(pat); // Guardamos el largo de pat
@@ -47,66 +44,56 @@ void eliminar(char *str, char *pat){
   *p1 = '\0';
 }
 
-// Entrega un nuevo string en donde
-// se han eliminado todas las ocurrencias de pat en str
 char *eliminados(char *str, char *pat) {
   int len = strlen(pat); // Guardamos el largo de pat
   char *p1 = str;
   char *p2 = p1;
   char *p3 = p1;
   char *puntero_pat = pat;
-  int count = 0; // Primero contaremos la cantidad de caracteres para hacer el malloc
+  int count = 0;
 
-  while (*p2) {
-    int match = 1; // Booleano que indica si hay match o no
+  while (*p2){
+    int match = 1; // bool si hay match o no
 
-    puntero_pat = pat;
-    for (int i = 0; i < len; i++) {
-      if (*p2 != *puntero_pat) {
+    for(int i = 0; i < len; i++){ // Recorremos el pat (puntero_pat) y vemos si hay match
+      if (*p2 != *puntero_pat){ // Si no hay match
         match = 0;
         break;
       }
       p2++;
       puntero_pat++;
     }
-
+    
     // Si hay match vamos a saltarnos todo el pat
     if(match){
-      count++;
       p2 = p1 + len;
-      *p1 = *p2;
-      p1++;
+      p3 = p1;
+      while(*p2){
+        *p3 = *p2;
+        p3++;
+        p2++;
+      }
+      *p3 = '\0';
     }
-    p2 = p1;
-    puntero_pat = pat;
 
     p1++;
+    p2 = p1;
+    puntero_pat = pat;
   }
+  *p1 = '\0';
 
-  char *str2 = malloc(count + 1); // Alojamos la memoria
+  int len2 = strlen(str);
 
-  p2 = str;
-  char *p4 = str2;
+  char *str2 = malloc(len2 + 1); // Asigna memoria para la nueva cadena
+  char *p4 = str;
+  char *p5 = str2;
 
-  while (*p2) {
-    char *p3 = pat;
-    int match = 1;
-
-    for (int i = 0; i < len; i++) {
-      if (*p2 != *p3) {
-        match = 0;
-        break;
-      }
-      p2++;
-      p3++;
-    }
-    if (!match){
-      *p4 = *p2;
-      p2++;
-      p4++;
-    }
+  while (*p4){
+    *p5 = *p4;
+    p4++;
+    p5++;
   }
-  *p4 = '\0';
   return str2;
 }
+
 
