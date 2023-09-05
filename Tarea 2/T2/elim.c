760d77717f7f63c89bee23d5d1e261548ec2dfc3
MartinEBravo <martin16bravo@gmail.com>
2023-09-05 09:11:24 -0700

    por fin ctmre

--- Tarea 2/T2/elim.c
+++ Tarea 2/T2/elim.c
@@ -4,48 +4,45 @@
 
 #include "elim.h"
 
+void eliminar(char *str, char *pat);
+char *eliminados(char *str, char *pat);
 
 // Elimina todas las ocurrencias de pat en str
 void eliminar(char *str, char *pat){
-  int len = strlen(pat); // Guardamos el largo de pat
-  char *p1 = str;
-  char *p2 = p1;
-  char *p3 = p1;
-  char *puntero_pat = pat;
+	int len = strlen(pat); // Guardamos el largo de pat
+	char *p1 = str;
+	char *p2 = p1;
+	char *puntero_pat = pat;
 
-  while (*p1){
-    int match = 1; // bool si hay match o no
+	while (*p1){
 
-    for(int i = 0; i < len; i++){ // Recorremos el pat (puntero_pat) y vemos si hay match
-      if (*p2 != *puntero_pat || *p2 == '\0'){
-        match = 0;
-        break;
-      }
-      p2++;
-      puntero_pat++;
-    }
-    
-    // Si hay match vamos a saltarnos todo el pat
-    if(match){
-      p2 = p1 + len;
-      p3 = p1;
-      while(*p2){
-        *p3 = *p2;
-        p3++;
-        p2++;
-      }
-      p2 = p1;
-      *p3 = '\0';
-      puntero_pat = pat;
-    }
-    // Si no hay match vamos a avanzar el puntero y reiniciar los punteros
-    else{
-      p1++;
-      p2 = p1;
-      puntero_pat = pat;
-    }
-  }
-  *p1 = '\0';
+		p2 = p1;
+		puntero_pat = pat;
+		
+		int match = 1;
+
+		for(int i = 0; i < len; i++){ // Recorremos el pat (puntero_pat) y vemos si hay match
+			if (*p2 != *puntero_pat || *p2 == '\0'){
+				match = 0;
+				break;
+			}
+			p2++;
+			puntero_pat++;
+		}
+
+		// Si hay match vamos a saltarnos todo el pat
+		if(match){
+			p1 += len;
+			
+		}
+		// Si no hay match vamos a avanzar el puntero y reiniciar los punteros
+		else{
+			*str = *p1;
+			p1++;
+			str++;
+		}
+	}
+	*str = 0;
 }
 
 char *eliminados(char *str, char *pat) {
@@ -80,13 +77,12 @@ char *eliminados(char *str, char *pat) {
       puntero_pat = pat;
     }
   }
-  *p1 = '\0';
 
   char *str2 = malloc(count + 1); // Asigna memoria para la nueva cadena
   p1 = str;
   p2 = str;
   char *p3 = str2;
-  char *p4 = str2;
+
   puntero_pat = pat;
 
   while(*p1){
@@ -109,17 +105,12 @@ char *eliminados(char *str, char *pat) {
       puntero_pat = pat;
     }
     else{
-      p2 = p1;
-      p4 = p3;
-      while(*p2 && *p2 != '\0'){
-        *p4 = *p2;
-        p2++;
-        p4++;
-      }
-      p1++;
-      p2 = p1;
-      p3++;
-      puntero_pat = pat;
+	*p3 = *p1;
+	p1++;
+	p3++;
+	p2 = p1;
+	puntero_pat = pat;
+		
     }
   }
   *p3 = '\0';
