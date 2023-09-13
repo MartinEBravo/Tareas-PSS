cb13a84770479b63f546d2fe48e64c11454c38fe
MartinEBravo <martin16bravo@gmail.com>
2023-09-13 14:11:02 -0700

    T3 lista

--- Tarea 3/prev.c
+++ Tarea 3/prev.c
@@ -3,10 +3,22 @@
 #include "prev.h"
 
 void asignarPrev(Nodo *t, Nodo **pprev) {
+  
+  // Abordamos caso árbol vacio
+  if (t == NULL) 
+    return;
+  // Recorremos árbol izquierdo
+  if (t->izq != NULL) 
+    asignarPrev(t->izq, pprev);
+
+  // Asignamos previo y prox
   t->prox = NULL;
-  if (*pprev != NULL) (*pprev)->prox = t;
+  if (*pprev != NULL) 
+    (*pprev)->prox = t;
   t->prev = *pprev;
   *pprev = t;
-  if (t->izq != NULL) asignarPrev(t->izq, pprev);
-  if (t->der != NULL) asignarPrev(t->der, pprev);
+
+  // Recorremos árbol derecho
+  if (t->der != NULL) 
+    asignarPrev(t->der, pprev);
 }
